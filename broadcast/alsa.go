351f0fb2fe9d2ef0c1a309aff0dc0b219f908430
Alban Peignier <alban@tryphon.eu>
2013-07-02 20:19:38 +0200

    Fixes AlsaSink Init()

--- broadcast/alsa.go
+++ broadcast/alsa.go
@@ -13,7 +13,9 @@ type AlsaSink struct {
 
 func (sink *AlsaSink) Init() error {
 	err := sink.handle.Open("default", alsa.StreamTypePlayback, alsa.ModeBlock)
-	return err
+	if err != nil {
+		return err
+	}
 
 	sink.handle.SampleFormat = alsa.SampleFormatS16LE
 	sink.handle.SampleRate = 44100
@@ -25,9 +27,11 @@ func (sink *AlsaSink) Init() error {
 
 func (alsa *AlsaSink) AudioOut(audio *Audio) {
 	pcmBytes := audio.PcmBytes()
+
 	alsaWriteLength, err := alsa.handle.Write(pcmBytes)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Write alsa buffer ", err.Error())
+		fmt.Fprintf(os.Stderr, "Can't write alsa buffer ", err.Error(), "\n")
+		return
 	}
 
 	wroteSamples := int64(alsaWriteLength / len(pcmBytes) * audio.sampleCount)
@@ -35,7 +39,7 @@ func (alsa *AlsaSink) AudioOut(audio *Audio) {
 	alsa.sampleCount += wroteSamples
 
 	if alsaWriteLength != len(pcmBytes) {
-		fmt.Fprintf(os.Stderr, "Did not write whole alsa buffer (Wrote %v, expected %v)\n", alsaWriteLength, pcmBytes)
+		fmt.Fprintf(os.Stderr, "Did not write whole alsa buffer (Wrote %v, expected %v)\n", alsaWriteLength, len(pcmBytes))
 	}
 
 	// fmt.Printf("%v alsa sampleCount : %d\n", time.Now(), alsa.sampleCount)
