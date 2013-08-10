bd6e2c74b8584cee019d3bee3f22a4e0f74f6e70
Alban Peignier <alban@tryphon.eu>
2013-08-10 14:06:11 +0200

    Move current http code in httpclient subcommand

--- go-broadcast.go
+++ go-broadcast.go
@@ -10,42 +10,59 @@ import (
 )
 
 func main() {
+	var command string
+	if len(os.Args) > 1 {
+		command = os.Args[1]
+	}
+
+	switch command {
+	case "httpclient":
+		httpClient(os.Args[2:])
+	default:
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] httpclient|... <url>\n", os.Args[0])
+		os.Exit(1)
+	}
+}
+
+func httpClient(arguments []string) {
 	var sampleRate uint = 44100
+
+	flags := flag.NewFlagSet("httpclient", flag.ExitOnError)
 
 	var lowAdjustLimit, lowAdjustThreshold, lowRefillMin, highAdjustLimit, highAdjustThreshold, highUnfillMax, highUnfill float64
 	var statusLoop, httpReadTimeout, httpWaitOnError time.Duration
 	var httpUsername, httpPassword string
 
-	flag.Float64Var(&lowAdjustLimit, "low-adjust-limit", 0, "Limit of low adjust buffer (in seconds)")
-	flag.Float64Var(&lowAdjustThreshold, "low-adjust-threshold", 3, "Limit of low adjust buffer (in seconds)")
-	flag.Float64Var(&lowRefillMin, "low-refill", 3, "Duration to refill when buffer is empty (in seconds)")
+	flags.Float64Var(&lowAdjustLimit, "low-adjust-limit", 0, "Limit of low adjust buffer (in seconds)")
+	flags.Float64Var(&lowAdjustThreshold, "low-adjust-threshold", 3, "Limit of low adjust buffer (in seconds)")
+	flags.Float64Var(&lowRefillMin, "low-refill", 3, "Duration to refill when buffer is empty (in seconds)")
 
-	flag.Float64Var(&highAdjustThreshold, "high-adjust-threshold", 7, "Limit of high adjust buffer (in seconds)")
-	flag.Float64Var(&highAdjustLimit, "high-adjust-limit", 10, "Limit of high adjust buffer (in seconds)")
-	flag.Float64Var(&highUnfillMax, "high-max", 10, "Max duration of buffer (in seconds)")
-	flag.Float64Var(&highUnfill, "high-unfill", 3, "Duration to unfill when buffer is full (in seconds)")
+	flags.Float64Var(&highAdjustThreshold, "high-adjust-threshold", 7, "Limit of high adjust buffer (in seconds)")
+	flags.Float64Var(&highAdjustLimit, "high-adjust-limit", 10, "Limit of high adjust buffer (in seconds)")
+	flags.Float64Var(&highUnfillMax, "high-max", 10, "Max duration of buffer (in seconds)")
+	flags.Float64Var(&highUnfill, "high-unfill", 3, "Duration to unfill when buffer is full (in seconds)")
 
-	flag.DurationVar(&statusLoop, "status-loop", 0, "Duration between two status dump (0 to disable)")
+	flags.DurationVar(&statusLoop, "status-loop", 0, "Duration between two status dump (0 to disable)")
 
-	flag.DurationVar(&httpReadTimeout, "http-read-timeout", 10*time.Second, "Read timeout before creating a new http connection")
-	flag.DurationVar(&httpWaitOnError, "http-wait-on-error", 5*time.Second, "Delay after http error")
+	flags.DurationVar(&httpReadTimeout, "http-read-timeout", 10*time.Second, "Read timeout before creating a new http connection")
+	flags.DurationVar(&httpWaitOnError, "http-wait-on-error", 5*time.Second, "Delay after http error")
 
-	flag.StringVar(&httpUsername, "http-username", "", "Username used for http authentification")
-	flag.StringVar(&httpPassword, "http-password", "", "Password used for http authentification")
+	flags.StringVar(&httpUsername, "http-username", "", "Username used for http authentification")
+	flags.StringVar(&httpPassword, "http-password", "", "Password used for http authentification")
 
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [options] <url>\n", os.Args[0])
-		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] httpclient <url>\n", os.Args[0])
+		flags.PrintDefaults()
 	}
 
-	flag.Parse()
+	flags.Parse(arguments)
 
-	if flag.NArg() != 1 {
+	if flags.NArg() != 1 {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	httpInput := broadcast.HttpInput{Url: flag.Arg(0), ReadTimeout: httpReadTimeout, WaitOnError: httpWaitOnError, Username: httpUsername, Password: httpPassword}
+	httpInput := broadcast.HttpInput{Url: flags.Arg(0), ReadTimeout: httpReadTimeout, WaitOnError: httpWaitOnError, Username: httpUsername, Password: httpPassword}
 	err := httpInput.Init()
 	checkError(err)
 
